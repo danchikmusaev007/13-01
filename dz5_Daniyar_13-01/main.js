caa5f1a8e10849813c0295ed20576d7c00aa4a4f
Daniar <{ID}+{username}@users.noreply.github.com>
2022-04-22 01:43:52 +0600

    dz5

--- dz5_Daniyar_13-01/main.js
+++ dz5_Daniyar_13-01/main.js
@@ -5,15 +5,15 @@ switch (lol) {
         document.getElementsByClassName('red_inner')[0].innerText = 'stop'
 		alert('stop_please')
         break;
+     case 'yellow':
+        document.getElementsByClassName('yellow_inner')[0].style.background = 'yellow'
+        document.getElementsByClassName('yellow_inner')[0].innerText = 'wait'
+		alert('wait_please')
+        break;
     case 'green':
         document.getElementsByClassName('green_inner')[0].style.background = 'green'
         document.getElementsByClassName('green_inner')[0].innerText = 'go'
 		alert('you can')
-        break;
-    case 'yellow':
-        document.getElementsByClassName('yellow_inner')[0].style.background = 'yellow'
-        document.getElementsByClassName('yellow_inner')[0].innerText = 'wait'
-		alert('wait_please')
         break;
     default:
         alert('undefined')
