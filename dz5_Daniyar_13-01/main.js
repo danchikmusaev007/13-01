4b4cbf89533649cb4f37099f92c4ea17df3f9104
Daniar <{ID}+{username}@users.noreply.github.com>
2022-04-22 00:54:48 +0600

    dz

--- dz5_Daniyar_13-01/main.js
+++ dz5_Daniyar_13-01/main.js
@@ -1,21 +1,15 @@
-const plus = document.getElementById('plus')
-const minus = document.getElementById('minus')
-const schetchik = document.getElementById('schetchik')
-const reset = document.getElementById('reset')
-let num = 0 ;
-
-plus.onclick = () => {
-    num++
-    schetchik.innerText = num.toString()
-}
-minus.onclick = () => {
-    num--
-    if (num < 1){
-        num = 0
-    }
-    schetchik.innerText = num.toString()
-}
-reset.onclick = () => {
-    num = 0
-    schetchik.innerText = num.toString()
+const lol = prompt('colour')
+switch (lol) {
+    case 'red':
+        document.getElementsByClassName('red_inner')[0].style.background = 'red'
+        break;
+    case 'green':
+        document.getElementsByClassName('green_inner')[0].style.background = 'green'
+        break;
+    case 'yellow':
+        document.getElementsByClassName('yellow_inner')[0].style.background = 'yellow'
+        break;
+    default:
+        alert('undefined')
+        break;
 }
