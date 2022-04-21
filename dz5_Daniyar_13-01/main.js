785aac22bea0c281acc76a5733141cabe6e8e105
Daniar <{ID}+{username}@users.noreply.github.com>
2022-04-22 01:41:00 +0600

    dz

--- dz5_Daniyar_13-01/main.js
+++ dz5_Daniyar_13-01/main.js
@@ -2,12 +2,18 @@ const lol = prompt('colour')
 switch (lol) {
     case 'red':
         document.getElementsByClassName('red_inner')[0].style.background = 'red'
+        document.getElementsByClassName('red_inner')[0].innerText = 'stop'
+		alert('stop_please')
         break;
     case 'green':
         document.getElementsByClassName('green_inner')[0].style.background = 'green'
+        document.getElementsByClassName('green_inner')[0].innerText = 'go'
+		alert('you can')
         break;
     case 'yellow':
         document.getElementsByClassName('yellow_inner')[0].style.background = 'yellow'
+        document.getElementsByClassName('yellow_inner')[0].innerText = 'wait'
+		alert('wait_please')
         break;
     default:
         alert('undefined')
