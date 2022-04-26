330fcc85e470c971e3fc8c11302149cd00631c14
Daniar <{ID}+{username}@users.noreply.github.com>
2022-04-26 22:59:46 +0600

    dz

---
+++ dz6_Daniyar_13-01/main.js
@@ -0,0 +1,7 @@
+	const mass1 = [1, 2, 3, 4, 5, 6];
+	const mass2 = [1, 2, 3, 4, 5];
+	const mass3 = [1, 2, 3, 4];
+	const mass4 = [1, 2, 3];
+	const mass6 = [1, 2];
+	const example = [...mass1, mass2,mass3,mass4,mass6];
+	console.log(example)
---
+++ dz6_Daniyar_13-01/script.js
@@ -0,0 +1,61 @@
+const obj = {
+	name: 'Danchik',
+	game: 'dota',
+	rang: '80',
+	scin: 'only_baba_skin',
+	team: 'tima_rakov',
+}
+const New_obj = {
+	...obj,
+	key3: 'fortnite',
+	key1: 'warface',
+	key2: 'genshin'
+}
+console.log(New_obj)
+
+
+const list = [
+
+]
+
+function render() {
+	const exampleDiv = document.createElement('div');
+	exampleDiv.setAttribute("class", "list");
+	for(let i = 0; i < list.length; i++ ) {
+			const divList = document.createElement('div');
+			divList.setAttribute("class", "burger");
+			const p = document.createElement("p");
+			p.innerHTML = list[i].text;
+			divList.append(p);
+			exampleDiv.append(divList);
+	}
+	const form = document.querySelector(".example");
+	document.querySelector(".list").remove();
+	form.append(exampleDiv)
+}
+btn.onclick = function() {
+	const name = document.getElementById('nick_name')
+	const obj = {
+			id: list.length,
+			text: "Hi  " + name.value
+	}
+	this.append(list);
+	list.push(obj);
+	render()
+}
+
+
+
+
+
+//"use strict";
+//const nameField = document.getElementById('name');
+//const result = document.getElementById('message');
+//
+//function showMessage() {
+  //result.value = `как ты братишка?, ${nameField.value}!`;
+//}
+//
+//nameField.addEventListener('reade', showMessage);
+
+
